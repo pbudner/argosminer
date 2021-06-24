078f45e21ec40ade61a5ac82595e7484ea71b144
Pascal Budner <mail@pascalbudner.de>
2021-06-24 18:53:31 +0200

    Prototyping InfluxDB & Redis connection

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 )
 
+var receivedEvents = prometheus.NewCounter(prometheus.CounterOpts{
+	Subsystem: "argosminer",
+	Name:      "received_events",
+	Help:      "Total number of received events.",
+})
+
+var ctx = context.Background()
+
+func init() {
+	prometheus.MustRegister(receivedEvents)
+}
+
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
-	fmt.Print("Hello World!")
+	influx := influxdb2.NewClientWithOptions("http://localhost:8086", "-EuU9TmvtAr27Cr_3bJvakriAVr7RNS04TsF_xD35-1XWZpog7Iz9dubOQMsCX9NUpRskLlHZSnhEZKvwineog==", influxdb2.DefaultOptions())
+	// Use blocking write client for writes to desired bucket
+	writeAPI := influx.WriteAPI("ciis", "go_test")
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		receivedEvents.Inc()
+		counter, err := rdb.Incr(ctx, "exemplary_dfg").Result()
+		line := fmt.Sprintf("my_counter,app=go counter=%d", counter)
+		writeAPI.WriteRecord(line)
+		if err != nil {
+			log.Panic(err)
+		}
+		c.JSON(200, gin.H{
+			"message": counter,
+		})
+	})
+
+	r.GET("/counter", func(c *gin.Context) {
+		receivedEvents.Inc()
+		counter, err := rdb.Get(ctx, "exemplary_dfg").Int64()
+		if err != nil {
+			log.Panic(err)
+		}
+		c.JSON(200, gin.H{
+			"counter": counter,
+		})
+	})
+
+	r.GET("/metrics", prometheusHandler())
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
