8559b959d4b992afc9b21fbfde52ce92fbf569cd
Pascal Budner <mail@pascalbudner.de>
2021-12-08 16:24:14 +0100

    Upgrade versions

--- parsers/json.go
+++ parsers/json.go
@@ -20,6 +20,18 @@ type JsonParserConfig struct {
 		Value     string `yaml:"value"`
 	} `yaml:"ignore-when"`
 }
+type JsonEventParserConfig struct {
+	ActivityPath          string `yaml:"activity-path"`
+	ProcessInstanceIdPath string `yaml:"process-instance-id-path"`
+	TimestampPath         string `yaml:"timestamp-path"`
+	TimestampFormat       string `yaml:"timestamp-format"`      // https://golang.org/src/time/format.go
+	TimestampTzIanakey    string `yaml:"timestamp-tz-iana-key"` // https://golang.org/src/time/format.go
+	IgnoreWhen            []struct {
+		Path      string `yaml:"path"`
+		Condition string `yaml:"condition"`
+		Value     string `yaml:"value"`
+	} `yaml:"ignore-when"`
+}
 
 type jsonParser struct {
 	config          JsonParserConfig
