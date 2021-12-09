e94a9d1595b6ce59ae562e9a9fdff9178a99a737
Pascal Budner <mail@pascalbudner.de>
2021-12-09 11:15:01 +0100

    Add simple JSON parser unit test

---
+++ parsers/json_test.go
@@ -0,0 +1,38 @@
+package parsers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJsoninJsonParser(t *testing.T) {
+	input := "{\"my_json\": \"{\\\"activity\\\": \\\"my_activity\\\", \\\"process_instance_id\\\": \\\"my_instance\\\", \\\"@timestamp\\\": \\\"2021-11-22T12:13:14.000\\\", \\\"inner_attribute\\\": \\\"foo\\\"}\", \"outer_attribute\": \"bar\"}"
+	parser := NewJsonParser(JsonParserConfig{
+		JsonPath:              "my_json",
+		ActivityPath:          "activity",
+		ProcessInstanceIdPath: "process_instance_id",
+		TimestampPath:         "@timestamp",
+		TimestampFormat:       "2006-01-02T15:04:05.000",
+		TimestampTzIanakey:    "Europe/Berlin",
+	})
+	evt, err := parser.Parse([]byte(input))
+	require.NoError(t, err)
+	require.EqualValues(t, "my_instance", evt.ProcessInstanceId)
+	require.EqualValues(t, "my_activity", evt.ActivityName)
+	require.EqualValues(t, 2021, evt.Timestamp.Year())
+
+	input2 := "{\"activity\": \"my_activity\", \"process_instance_id\": \"my_instance\", \"@timestamp\": \"2021-11-22T12:13:14.000\", \"inner_attribute\": \"foo\"}"
+	parser2 := NewJsonParser(JsonParserConfig{
+		ActivityPath:          "activity",
+		ProcessInstanceIdPath: "process_instance_id",
+		TimestampPath:         "@timestamp",
+		TimestampFormat:       "2006-01-02T15:04:05.000",
+		TimestampTzIanakey:    "Europe/Berlin",
+	})
+	evt2, err := parser2.Parse([]byte(input2))
+	require.NoError(t, err)
+	require.EqualValues(t, "my_instance", evt2.ProcessInstanceId)
+	require.EqualValues(t, "my_activity", evt2.ActivityName)
+	require.EqualValues(t, 2021, evt2.Timestamp.Year())
+}
