48041f23c568f111738652aebe86bafd41c046ca
Pascal Budner <mail@pascalbudner.de>
2022-02-24 12:09:06 +0100

    Implement an Event Buffer

---
+++ pipeline/transforms/event_buffer.go
@@ -0,0 +1,130 @@
+package transforms
+
+import (
+	"container/heap"
+	"context"
+	"sync"
+	"time"
+
+	"github.com/labstack/gommon/log"
+	"github.com/pbudner/argosminer/pipeline"
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
+)
+
+type EventBufferConfig struct {
+	MaxEvents             int           `yaml:"max-events"`
+	MaxAge                time.Duration `yaml:"max-age"`
+	FlushInterval         time.Duration `yaml:"flush-interval"`
+	IgnoreEventsOlderThan time.Duration `yaml:"ignore-events-older-than"`
+}
+
+type eventBuffer struct {
+	pipeline.Publisher
+	pipeline.Consumer
+	config EventBufferConfig
+	buffer timeOrderedEventBuffer
+	log    *zap.SugaredLogger
+}
+
+var (
+	eventBufferCurrentItems = prometheus.NewGauge(prometheus.GaugeOpts{
+		Subsystem: "argosminer_event_buffer",
+		Name:      "current_events",
+		Help:      "Number of current events in buffer.",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(eventBufferCurrentItems)
+}
+
+func NewEventBuffer(config EventBufferConfig) *eventBuffer {
+	return &eventBuffer{
+		log:    zap.L().Sugar().With("service", "event-buffer"),
+		config: config,
+	}
+}
+
+func (eb *eventBuffer) Run(wg *sync.WaitGroup, ctx context.Context) {
+	eb.log.Info("Starting pipeline.transforms.EventBuffer")
+	wg.Add(1)
+	defer wg.Done()
+	ticker := time.NewTicker(eb.config.FlushInterval)
+	heap.Init(&eb.buffer)
+	var counter uint64 = 0
+	for {
+		select {
+		case <-ctx.Done():
+			ticker.Stop()
+			eb.log.Info("Shutting down pipeline.transforms.EventBuffer")
+			eb.flush(true)
+			return
+		case <-ticker.C:
+			eb.flush(false)
+		case input := <-eb.Consumes:
+			evt := input.(pipeline.Event)
+			if -time.Until(evt.Timestamp) > eb.config.IgnoreEventsOlderThan {
+				eb.Consumes <- false
+				continue
+			}
+			heap.Push(&eb.buffer, &eventBufferItem{
+				value:   evt,
+				time:    evt.Timestamp.UnixNano(),
+				counter: counter,
+			})
+			counter++
+			eventBufferCurrentItems.Inc()
+			eb.Consumes <- true
+			eb.flush(false)
+		}
+	}
+}
+
+func (eb *eventBuffer) flush(all bool) {
+	// flush aged items or the oldest items if we have too many
+	for all || (eb.buffer.Len() > eb.config.MaxEvents || (eb.buffer.Len() > 0 && -time.Until(eb.buffer[0].value.Timestamp) > eb.config.MaxAge)) {
+		log.Info("Found an outaged item, flushing it now")
+		evt := heap.Pop(&eb.buffer).(*eventBufferItem).value
+		eventBufferCurrentItems.Dec()
+		eb.Publish(evt, true)
+	}
+}
+
+type eventBufferItem struct {
+	value   pipeline.Event
+	time    int64
+	index   int
+	counter uint64
+}
+
+type timeOrderedEventBuffer []*eventBufferItem
+
+func (pq timeOrderedEventBuffer) Len() int { return len(pq) }
+
+func (pq timeOrderedEventBuffer) Less(i, j int) bool {
+	return (pq[i].time == pq[j].time && pq[i].counter < pq[j].counter) || pq[i].time < pq[j].time // preserves order
+}
+
+func (pq timeOrderedEventBuffer) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *timeOrderedEventBuffer) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*eventBufferItem)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *timeOrderedEventBuffer) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
---
+++ pipeline/transforms/event_buffer_test.go
@@ -0,0 +1,110 @@
+package transforms
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/pbudner/argosminer/pipeline"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBuffer(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	ctx := context.Background()
+	buffer := NewEventBuffer(EventBufferConfig{
+		MaxEvents:             5,
+		MaxAge:                500 * time.Millisecond,
+		FlushInterval:         100 * time.Millisecond,
+		IgnoreEventsOlderThan: 1 * time.Minute,
+	})
+	evts := make(chan interface{})
+	buffer.Link(evts)
+	receiver := buffer.Subscribe()
+	go buffer.Run(wg, ctx)
+
+	start := time.Now()
+	evts <- pipeline.Event{
+		ActivityName:      "Activity A",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         time.Now(),
+	}
+	require.EqualValues(t, true, <-evts)
+	evt := (<-receiver).(pipeline.Event)
+	require.GreaterOrEqual(t, -time.Until(start), 5*time.Millisecond)
+	require.EqualValues(t, "Activity A", evt.ActivityName)
+
+	now := time.Now()
+	evts <- pipeline.Event{
+		ActivityName:      "Activity B",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now,
+	}
+	require.EqualValues(t, true, <-evts)
+
+	evts <- pipeline.Event{
+		ActivityName:      "Activity C",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now.Add(-100 * time.Millisecond),
+	}
+	require.EqualValues(t, true, <-evts)
+
+	evts <- pipeline.Event{
+		ActivityName:      "Activity D",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now.Add(100 * time.Millisecond),
+	}
+	require.EqualValues(t, true, <-evts)
+
+	evts <- pipeline.Event{
+		ActivityName:      "Activity E",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now.Add(-200 * time.Millisecond),
+	}
+	require.EqualValues(t, true, <-evts)
+
+	evts <- pipeline.Event{
+		ActivityName:      "Activity F",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now.Add(200 * time.Millisecond),
+	}
+	require.EqualValues(t, true, <-evts)
+
+	evts <- pipeline.Event{
+		ActivityName:      "Activity G",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now.Add(200 * time.Millisecond),
+	}
+	require.EqualValues(t, true, <-evts)
+
+	evts <- pipeline.Event{
+		ActivityName:      "Activity Invalid",
+		ProcessInstanceId: "ID-4711",
+		Timestamp:         now.Add(-2 * time.Minute),
+	}
+	require.EqualValues(t, false, <-evts) // reject event
+
+	evt = (<-receiver).(pipeline.Event)
+	require.EqualValues(t, "Activity E", evt.ActivityName)
+	evt = (<-receiver).(pipeline.Event)
+	require.EqualValues(t, "Activity C", evt.ActivityName)
+	evt = (<-receiver).(pipeline.Event)
+	require.EqualValues(t, "Activity B", evt.ActivityName)
+	evt = (<-receiver).(pipeline.Event)
+	require.EqualValues(t, "Activity D", evt.ActivityName)
+	evt = (<-receiver).(pipeline.Event)
+	require.EqualValues(t, "Activity F", evt.ActivityName)
+	evt = (<-receiver).(pipeline.Event)
+	require.EqualValues(t, "Activity G", evt.ActivityName)
+
+	// now check that "Activity invalid" is not published
+	wg2 := &sync.WaitGroup{}
+	wg2.Add(1)
+	go func() {
+		evt = (<-receiver).(pipeline.Event)
+		wg2.Done()
+	}()
+	wg2.Done() // this should not panic, because the goroutine should never finish
+	wg2.Wait() // this should not timeout the unit test
+}
